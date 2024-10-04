64a74648c0bf9f62a3ea02a9a907109320018647
Vladisalv60 <katerusin.k93@gmail.com>
2024-10-04 15:48:06 +0500

    new_config_setting

--- ktscrm/settings.py
+++ ktscrm/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['http://192.168.1.19:8008']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/

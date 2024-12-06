8411f2cbf424f2aba749ab23d33f8c72b75f5393
Vladisalv60 <katerusin.k93@gmail.com>
2024-12-06 18:09:54 +0500

    время заявки техникам11

--- dogovornoy/views.py
+++ dogovornoy/views.py
@@ -15048,9 +15048,6 @@ def button_handler(update, context):
                     date_event = row[8].strftime('%d-%m-%Y %H:%M:%S') if row[8] else 'Нет данных'
                     gprs_quality = row[15]
                     message += f"Раздел: {razdel}, Зона/Польз: {zona_user}, Событие: {event_str}, Дата: {date_event}, Качество: {gprs_quality}\n\n"
-                    # last_event_sn[task_id] = alarms[0][1]
-                # Сохраняем последний SN из полученных данных
-                print(f"SN из последнего события + {last_event_sn[task_id]}")
             else:
                 message = "События не найдены."
 
@@ -15061,7 +15058,7 @@ def button_handler(update, context):
             ]
 
             reply_markup = InlineKeyboardMarkup(keyboard)
-            if query.message.text != message or query.message.reply_markup != reply_markup:
+            if query.message.text != message:
                 query.edit_message_text(text=message, reply_markup=reply_markup)
             else:
                 print("Содержимое сообщения и разметка не изменились, обновление пропущено.")
@@ -15081,10 +15078,6 @@ def button_handler(update, context):
                     date_event = row[8].strftime('%d-%m-%Y %H:%M:%S') if row[8] else 'Нет данных'
                     gprs_quality = row[15]
                     message += f"Раздел: {razdel}, Зона/Польз: {zona_user}, Событие: {event_str}, Дата: {date_event}, Качество: {gprs_quality}\n\n"
-                    # last_event_sn[task_id] = new_alarms[0][1]
-
-                # Обновляем последний SN для последующих обновлений
-                # print(f"Обновленный last_event_sn = {last_event_sn[task_id]}")
             else:
                 message = "Нет новых событий для модуля."
 

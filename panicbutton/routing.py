c5c774eef3b15b5a0d2ce12a51a315620d3258db
Vladisalv60 <katerusin.k93@gmail.com>
2025-02-13 11:42:44 +0500

    Изменение карты8

--- panicbutton/routing.py
+++ panicbutton/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from panicbutton.consumers import *
+from panicbutton.consumers import AlarmConsumer
 
 websocket_urlpatterns = [
-    path("ws/alarms/", AlarmConsumer.as_asgi()),
+    path("ws/alarms/", AlarmConsumer.as_asgi()),  # 🔥 ВАЖНО! ws/alarms/
 ]
